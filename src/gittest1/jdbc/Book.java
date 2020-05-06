6b8a6bdd931cf3fcba28b633fd41dc497aa9e252
Jaden727 <joohyung727@gmail.com>
2020-05-06 12:40:37 +0900

    push

--- src/gittest1/jdbc/Book.java
+++ src/gittest1/jdbc/Book.java
@@ -1,5 +1,57 @@
 package gittest1.jdbc;
 
 public class Book {
+	int id;
+	String title;
+	String author;
+	String categoryId;
+	int price;
+	String publisher;
+	String categoryName;
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
 
-}
+
+	}
+
+
+	}
